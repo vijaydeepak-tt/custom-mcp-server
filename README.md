958594d4f88a01bf28a454e34a75a6767ab07720
Vijayasekhar Deepak <vijaydeepak7195@outlook.com>
2025-06-22 20:37:25 +0530

    custom mcp for AI Sticky Notes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Custom MCP Server
+
+Python implementation of the Model Context Protocol (MCP) - AI Sticky Notes
+
+### Resources Used:
+
+1. [MCP Python SDK](https://github.com/modelcontextprotocol/python-sdk) to build a MCP server with Python
+2. [UV](https://docs.astral.sh/uv/getting-started/installation/#installation-methods) for python project setup.
+3. [Claude Desktop](https://claude.ai/download)
---
+++ main.py
@@ -0,0 +1,72 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+import os
+
+# Create an MCP server
+mcp = FastMCP("AI Sticky Notes")
+
+NOTES_FILE = os.path.join(os.path.dirname(__file__), "notes.txt")
+
+def ensure_file():
+    if not os.path.exists(NOTES_FILE):
+        with open(NOTES_FILE, "w") as f:
+            f.write("")
+
+@mcp.tool()
+def add_note(message: str) -> str:
+    """
+    Append a new note to the sticky note file.
+
+    Args:
+        message(str): The note content that to be added.
+
+    Returns:
+        str: Confirmation message indication the note was saved.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "a") as f:
+        f.write(message + "\n")
+    return "Note Saved!"
+
+@mcp.tool()
+def read_notes() -> str:
+    """
+    Read and return all notes from the sticky note file.
+
+    Returns:
+        str: All notes as a single string separated by line breaks.
+             If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        content = f.read().strip()
+    return content or "No Notes added."
+
+@mcp.resource("notes://latest")
+def get_latest_note() -> str:
+    """
+    Get the most recently added note from the sticky note file.
+
+    Returns:
+        str: The last note entry. If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        lines = f.readlines()
+    return lines[-1] if lines else "No Notes added."
+
+@mcp.prompt()
+def note_summary_prompt() -> str:
+    """
+    Generate a prompt asking the AI to summarize all current notes.
+
+    Returns:
+        str: A prompt string that includes all notes and asks for a summary.
+             If no notes exist, a message will be shown indicating that.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        content = f.read().strip()
+    if not content:
+        return "No Note added Yet!"
+    return f"Summarize the content notes {content}"
